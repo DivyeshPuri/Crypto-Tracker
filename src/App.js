522e7696a1d51f003958c6cb47ffba0215f481e0
Divyesh Puri <divyeshpuri66@gmail.com>
2018-05-13 20:38:37 +0530

    Adding Basic functions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    crypto: [];
+  }
+
+  componentDidMount() {
+    axios.get()
+    .then(res => {
+      const crypto = res.data;
+      console.log(crypto);
+      this.setState({crypto: crypto});
+    })
+  }
+
   render() {
     return (
       <div className="App">
