12203a47c8724df6bf89f36eb81dec6d4b20d94d
Lucifer24 <divyeshpuri66@gmail.com>
2018-05-13 20:20:41 +0530

    Adding Basic functionalities

--- crypto_tracker/src/App.js
+++ crypto_tracker/src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			crypto: []
+		}
+	}
+
+	componentDidMount() {
+		axios.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR')
+			.then(res => {
+				const crypto = res.data;
+				console.log(crypto);
+				this.setState({crypto: crypto});
+			})
+	}
+
+  	render() {
+    	return (
+      	<div className="App">
+    		<header className="App-header">
+          	<img src={logo} className="App-logo" alt="logo" />
+          	<h1 className="App-title">Welcome to React</h1>
+        	</header>
+        	<p className="App-intro">
+          		{Object.keys(this.state.crypto).map((key) => (
+					  	<div>
+								<span>{key}</span>
+								<span>{this.state.crypto[key]}</span>
+						</div>
+					))
+				}
+        	</p>
+      	</div>
+    	);
+  	}
 }
 
 export default App;
