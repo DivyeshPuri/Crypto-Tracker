ffadbbf473c3cd6effd214c64c3f6025e820673a
Divyesh Puri <divyeshpuri66@gmail.com>
2018-05-18 21:24:07 +0530

    Getting coinlist (#11)

--- src/App.js
+++ src/App.js
@@ -15,20 +15,25 @@ class App extends Component {
   }
 
   componentDidMount() {
+    axios.get('https://www.cryptocompare.com/api/data/coinlist/')
+    .then(res => {
+      const crypto_list = res.data;
+      this.setState({
+        crypto_list: crypto_list.Data,
+      });
+      var list = '';
+      for (var element in crypto_list.Data){
+        list = list + crypto_list.Data[element].Name + ',';
+        console.log(list);
+      };
+    })
+    
+
     axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=USD')
     .then(res => {
       const crypto = res.data;
       this.setState({
         crypto: crypto,
-      });
-    })
-
-    axios.get('https://www.cryptocompare.com/api/data/coinlist/')
-    .then(res => {
-      const crypto_list = res.data;
-      console.log(crypto_list);
-      this.setState({
-        crypto_list: crypto_list.Data,
       });
     })
   }
