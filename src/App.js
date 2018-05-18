c74b5538379bce7cc4c29bae7ba9ef3522bc208b
Lucifer24 <divyeshpuri66@gmail.com>
2018-05-19 02:11:15 +0530

    Showing results of first 40 Currencies

--- src/App.js
+++ src/App.js
@@ -5,56 +5,62 @@ import axios from 'axios';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state= {
-      crypto: [],
-      crypto_list: [],
-    };
-  }
+		this.state= {
+			crypto: [],
+			crypto_list: [],
+		};
+	}
 
-  componentDidMount() {
-    axios.get('https://www.cryptocompare.com/api/data/coinlist/')
-    .then(res => {
-      const crypto_list = res.data;
-      this.setState({
-        crypto_list: crypto_list.Data,
-      });
-      var list = '';
-      for (var element in crypto_list.Data){
-        list = list + crypto_list.Data[element].Name + ',';
-        console.log(list);
-      };
-    })
-    
+	componentDidMount() {
+		axios.get('https://www.cryptocompare.com/api/data/coinlist/')
+		.then(res => {
+			const crypto_list = res.data;
+			this.setState({
+				crypto_list: crypto_list.Data,
+			});
+			var list = '';
+			var count = 0;
+			for (var element in crypto_list.Data){
+				if(count < 40) {
+					list = list + crypto_list.Data[element].Name + ',';	
+				}
+				count++;
+			};
 
-    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=USD')
-    .then(res => {
-      const crypto = res.data;
-      this.setState({
-        crypto: crypto,
-      });
-    })
-  }
+			const url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=' + list + '&tsyms=USD';
+			console.log(url);
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+			axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=' + list + '&tsyms=USD')
+				.then(res => {
+					const crypto = res.data;
+					this.setState({
+						crypto: crypto,
+					});
+				})
+		})
+		
+	}
 
-        {Object.keys(this.state.crypto).map((key) => (
-          <div>
-            <span>{key}</span>
-            <span>{this.state.crypto[key].USD}</span>
-          </div>
-        ))}
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Welcome to React</h1>
+				</header>
+
+				{Object.keys(this.state.crypto).map((key) => (
+					<div>
+						<span>{key}</span>
+						<span>{this.state.crypto[key].USD}</span>
+					</div>
+				))}
+			</div>
+		);
+	}
 }
 
 export default App;
