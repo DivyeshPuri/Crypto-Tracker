7292655c65ae5a1e44619026ea6b34fae846fb13
Divyesh Puri <divyeshpuri66@gmail.com>
2018-12-11 00:15:45 +0530

    Removing import of semantic-ui (#51)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "semantic-ui-css/semantic.css";
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import UIkit from 'uikit';
