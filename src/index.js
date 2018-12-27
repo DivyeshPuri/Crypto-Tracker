b92feb86196801cf1ecc9978eab412b21ab9c868
Piyush Pawar <piyushpawar25@gmail.com>
2018-12-27 14:46:04 +0530

    UI Improvement and Bug Fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
